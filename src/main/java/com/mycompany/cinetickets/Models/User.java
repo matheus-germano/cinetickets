29ca6f4370340ea09a816c8b481762d565237648
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-10-30 15:56:15 -0300

    feat: User Model Class

---
+++ src/main/java/com/mycompany/cinetickets/Models/User.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cinetickets.Models;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author matheus
+ */
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+    
+    public User(String id, String name, String email, String password, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
