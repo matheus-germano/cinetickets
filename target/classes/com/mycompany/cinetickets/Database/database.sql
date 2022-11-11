349fb64ac43f82fc6768afe079cf321ac9b8b17e
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-11-11 15:12:41 -0300

    feat: adding poster column in movie table

--- target/classes/com/mycompany/cinetickets/Database/database.sql
+++ target/classes/com/mycompany/cinetickets/Database/database.sql
@@ -16,7 +16,8 @@ nome VARCHAR(45) NOT NULL,
 genero VARCHAR(45) NOT NULL,
 lancamento DATE NOT NULL,
 duracao TIME NOT NULL,
-classificacao enum ('Livre','10 anos','12 anos','14 anos','16 anos','18 anos')
+classificacao enum ('Livre','10 anos','12 anos','14 anos','16 anos','18 anos'),
+poster text
 );
 
 CREATE TABLE sala (
@@ -51,16 +52,16 @@ FOREIGN KEY (cpfCliente) REFERENCES cliente (cpf)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 );
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Sonic 2','Animação','2022-04-07', 20200,'10 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Doutor Estranho no Multiverso da Loucura','Fantasia','2022-05-05', 20600,'14 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Jujutsu Kaisen 0','Anime','2022-04-28', 14500,'14 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Medida Provisória','Drama','2022-04-14', 13400,'14 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Jurassic World: Domínio','Ficção Científica','2022-06-02', 22700,'12 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Cidade Perdida','Aventura','2022-04-21', 15200,'14 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('A Medium','Terror','2022-05-19', 21100,'16 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('D. P. A. 3 - Uma Aventura no Fim do Mundo','Comédia','2021-01-07', 10200,'Livre');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Batman','Ação','2022-03-03', 25600, '14 anos');
-insert into filme (nome, genero, lancamento, duracao, classificacao) values ('The Joker', 'Drama', '2019-10-04', 20200, '16 anos');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Sonic 2','Animação','2022-04-07', 20200,'10 anos','https://jovemnerd.com.br/wp-content/uploads/2022/02/Poster-de-Sonic-2-760x950.jpg');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Doutor Estranho no Multiverso da Loucura','Fantasia','2022-05-05', 20600,'14 anos', 'https://www.einerd.com.br/wp-content/uploads/2022/04/doutor-estranho-novo-poster-1.jpg.webp');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Jujutsu Kaisen 0','Anime','2022-04-28', 14500,'14 anos', 'https://d1fz32b7q4znht.cloudfront.net/wp-content/uploads/2021/10/Jujutsu-Kaisen-0-GQCA-poster.jpg');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Medida Provisória','Drama','2022-04-14', 13400,'14 anos', 'https://cineclick-static.flixmedia.cloud/1280/processed/518f41d8-1a6c-4692-8842-f806d194577d.webp');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Jurassic World: Domínio','Ficção Científica','2022-06-02', 22700,'12 anos', 'https://m.media-amazon.com/images/M/MV5BOTBjMjA4NmYtN2RjMi00YWZlLTliYTktOTIwMmNkYjYxYmE1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Cidade Perdida','Aventura','2022-04-21', 15200,'14 anos', 'https://br.web.img3.acsta.net/pictures/21/12/16/15/18/0828406.jpg');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('A Medium','Terror','2022-05-19', 21100,'16 anos', 'https://i0.wp.com/cebolaverde.com.br/wp-content/uploads/2022/05/Poster-Alternativo-A-Medium-65x95cm-scaled.jpg?ssl=1');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('D. P. A. 3 - Uma Aventura no Fim do Mundo','Comédia','2021-01-07', 10200,'Livre', 'https://s2.glbimg.com/pYtnpGa5Gg27TFnkG3Cj-o5PBtE=/e.glbimg.com/og/ed/f/original/2020/10/12/dpa3_final1_g.jpg');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('Batman','Ação','2022-03-03', 25600, '14 anos', 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg');
+insert into filme (nome, genero, lancamento, duracao, classificacao) values ('The Joker', 'Drama', '2019-10-04', 20200, '16 anos', 'https://http2.mlstatic.com/D_NQ_NP_972010-MLB47709378565_092021-W.jpg');
 
 insert into cliente values
 ('12345678900', 'Matheus Germano','2003-02-27', 'dev.mgermano@gmail.com', 'QE1nMjcwMjAzQA=='),
@@ -103,7 +104,7 @@ insert into sessao values
 select * from cliente;
 select * from filme;
 select * from sala;
-select * from sessao;
+select distinct numeroSala from sessao;
 select * from ticket;
 
 -- Consulta Nome, Preço e Data dos ingressos comprados
@@ -122,3 +123,5 @@ SELECT filme.nome as 'Nome do Filme', filme.genero as 'Gênero', date_format(fil
 	FROM filme right join ticket on filme.idFilme = ticket.idFilme
     group by ticket.idTicket
     order by ticket.idTicket asc;
+    
+    select * from cliente where email = 'dev.mgermano@gmail.com' and senha = 'QE1nMjcwMjAzQA=='limit 1;
